70f743fa816d2865f14c67809273b7966b5f7c9d
Dima <s02200136@gse.cs.msu.ru>
2021-02-24 13:48:35 +0300

    лишний import

--- 0/main.py
+++ 0/main.py
@@ -1,4 +1,3 @@
-import numpy as np
 import matplotlib.pyplot as plt
 from matrix_solver import gen_and_solve
 
@@ -6,8 +5,8 @@ if __name__ == '__main__':
 	sizes = []
 	times = []
 	for i in range(1, 4):
-		sol, A, b, time = gen_and_solve(10**i)
-		sizes.append(10**i)
+		sol, A, b, time = gen_and_solve(10 ** i)
+		sizes.append(10 ** i)
 		times.append(time)
 		print(time)
 	plt.plot(sizes, times)
