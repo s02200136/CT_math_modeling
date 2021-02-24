ad5a2fc637b243570420311c28fd8af2d03461f4
Dima <s02200136@gse.cs.msu.ru>
2021-02-24 14:09:37 +0300

    усреднение по времени

--- 0/main.py
+++ 0/main.py
@@ -4,11 +4,14 @@ from matrix_solver import gen_and_solve
 if __name__ == '__main__':
 	sizes = []
 	times = []
-	for i in range(1, 4):
-		sol, A, b, time = gen_and_solve(10 ** i)
+	for i in range(1, 5):
+		elapsed_time = 0
 		sizes.append(10 ** i)
-		times.append(time)
-		print(time)
+		for _ in range(5):
+			sol, A, b, time = gen_and_solve(10 ** i)
+			elapsed_time += time
+			print(time)
+		times.append(elapsed_time / 5)
 	plt.plot(sizes, times)
 	plt.legend("time")
 	plt.show()
